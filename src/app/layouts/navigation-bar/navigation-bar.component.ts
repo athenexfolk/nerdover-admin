f7fe71d9db1f460b856726abc872b821da23d332
athenexfolk <folkadpocket99@gmail.com>
2024-02-04 01:45:09 +0700

    add expand navbar

--- src/app/layouts/navigation-bar/navigation-bar.component.ts
+++ src/app/layouts/navigation-bar/navigation-bar.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.scss'
+  styleUrl: './navigation-bar.component.scss',
 })
 export class NavigationBarComponent {
+  isNavExpanded = false;
 
+  toggleExpand() {
+    this.isNavExpanded = !this.isNavExpanded;
+  }
 }
